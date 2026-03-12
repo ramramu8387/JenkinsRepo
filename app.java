35f53571f9631c7ae6f5baab25508c1cf35a6b0c
RAMA MOHAN BOLE <143954553+ramramu8387@users.noreply.github.com>
2026-03-12 13:54:16 +0300

    Update app.java

--- app.java
+++ app.java
@@ -34,3 +34,21 @@ public class Main {
     System.out.println("Number of digits: " + count);
   }
 }
+
+
+class Main {
+  public static void main(String[] args) {
+
+    int n = 10, firstTerm = 0, secondTerm = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
+
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(firstTerm + ", ");
+
+      // compute the next term
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+    }
+  }
+}
