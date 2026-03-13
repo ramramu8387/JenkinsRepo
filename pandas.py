9b94fb897ff00a8481b793b6708a136ef332f726
root <root@ip-172-31-93-225.ec2.internal>
2026-03-13 10:36:27 +0000

    pandas file for v2

--- pandas.py
+++ pandas.py
@@ -16,3 +16,13 @@ sol2 = (-b+cmath.sqrt(d))/(2*a)
 
 print('The solution are {0} and {1}'.format(sol1,sol2))
 
+
+# Python program to check if the input number is odd or even.
+# A number is even if division by 2 gives a remainder of 0.
+# If the remainder is 1, it is an odd number.
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
